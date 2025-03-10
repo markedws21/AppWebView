3f7bbb0a49ef7dbd4ce44fb22b623d3409295df3
AzureAD\MarkEdward <mespinozar@lucky.com.pe>
2025-03-10 11:39:31 -0500

    avance

---
+++ webview_app/lib/webview_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  WebViewScreenState createState() => WebViewScreenState();
+}
+
+class WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      //Iniciar Pagina web
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView')
+      ),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
