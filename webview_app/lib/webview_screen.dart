040afe1d1510638228bfa59529b1ae025f4bb51f
AzureAD\MarkEdward <mespinozar@lucky.com.pe>
2025-03-10 15:08:17 -0500

    avance 3

--- webview_app/lib/webview_screen.dart
+++ webview_app/lib/webview_screen.dart
@@ -10,58 +10,62 @@ class WebViewScreen extends StatefulWidget {
 
 class WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController _controller;
-  double _dragOffset = 0.0;
-  bool _showRefreshIcon = false;
+  bool isAtTop = true;
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse('https://mapbs2.mapsalud.com/clientes/lucky/web/public/'));
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) async {
+            // Verificar si la WebView está en la parte superior
+            final position = await _controller.runJavaScriptReturningResult("window.scrollY;");
+            setState(() {
+              isAtTop = position == 0;
+            });
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://grupolucky-intranet.azurewebsites.net/'));
   }
 
-  void _onVerticalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      _dragOffset += details.primaryDelta!;
-      _showRefreshIcon = _dragOffset > 30; // Mostrar icono si el usuario arrastra más de 30px
-    });
-  }
-
-  void _onVerticalDragEnd(DragEndDetails details) {
-    if (_showRefreshIcon) {
-      _controller.reload();
-    }
-    setState(() {
-      _dragOffset = 0;
-      _showRefreshIcon = false; // Oculta icono después de recargar
-    });
+  Future<void> _reloadPage() async {
+    await _controller.reload();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          GestureDetector(
-            onVerticalDragUpdate: _onVerticalDragUpdate,
-            onVerticalDragEnd: _onVerticalDragEnd,
-            child: WebViewWidget(controller: _controller),
-          ),
-          AnimatedOpacity(
-            opacity: _showRefreshIcon ? 1.0 : 0.0,
-            duration: const Duration(milliseconds: 200),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 50.0),
-              child: Icon(
-                Icons.refresh,
-                color: Colors.white,
-                size: 40,
-              ),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title:Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Intranet Lucky',style: TextStyle(color: Colors.white),),
+            IconButton(onPressed: _reloadPage, icon: const Icon(Icons.refresh,color: Colors.white,))
+          ],
+        )
+      ),
+      body: RefreshIndicator(
+        onRefresh: isAtTop ? _reloadPage : () async {},
+        child: NotificationListener<ScrollUpdateNotification>(
+          onNotification: (scrollNotification) {
+            if (scrollNotification.metrics.pixels <= 0) {
+              setState(() {
+                isAtTop = true;
+              });
+            } else {
+              setState(() {
+                isAtTop = false;
+              });
+            }
+            return false;
+          },
+          child: WebViewWidget(controller: _controller),
+        ),
       ),
     );
   }
